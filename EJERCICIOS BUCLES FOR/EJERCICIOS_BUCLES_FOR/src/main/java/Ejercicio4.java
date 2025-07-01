2a9c4a4105ce4594c6cf5ed7c4452fc51b077714
lalopgar <laralg90@gmail.com>
2025-07-01 18:43:56 +0200

    Ejercicio 4 creo la variable tabla del 7 y un bucle que recorra del 1 al 10

--- EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio4.java
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio4.java
@@ -1,4 +1,11 @@
 //Imprimir una tabla de multiplicar (por ejemplo del 7)
 
 public class Ejercicio4 {
+    public static void main(String[] args) {
+
+        int tabla = 7;
+        for (int i=1;i<=10;i++){
+
+        }
+    }
 }
--- EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio5.java
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio5.java
@@ -1,4 +1,5 @@
 //Contar cuántos múltiplos de 3 hay entre 1 y 100
 
 public class Ejercicio5 {
+
 }
