a5f4a98773a77d6752ed77a15cdf1fc1febbf3c5
lalopgar <laralg90@gmail.com>
2025-07-01 20:54:07 +0200

    Ejercicio 7 primero declaro las variables

--- EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio7.java
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio7.java
@@ -1,4 +1,11 @@
 //Mostrar los primeros 10 números de Fibonacci
 
 public class Ejercicio7 {
+    public static void main(String[] args) {
+
+        int a = 0; //primer nº de la secuencia
+        int b = 1; //segundo nº de la secuencia
+
+
+    }
 }
