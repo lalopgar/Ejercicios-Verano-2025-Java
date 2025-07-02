082e62656bc158d84ddcc9389940981126113a48
lalopgar <laralg90@gmail.com>
2025-07-02 19:35:31 +0200

    Ejercicio 9 añado número de filas(5)

--- EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio6.java
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio6.java
@@ -1,4 +1,14 @@
 //Factorial de un número n (ej. 5! = 5×4×3×2×1)
 
+import java.util.Scanner;
+
 public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce un número: ");
+        int num = teclado.nextInt();
+
+
+    }
 }
--- EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio9.java
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio9.java
@@ -3,6 +3,9 @@
 public class Ejercicio9 {
     public static void main(String[] args) {
 
+        for (int i=1;i<=5;i++){ //filas
+
+        }
     }
 }
 
