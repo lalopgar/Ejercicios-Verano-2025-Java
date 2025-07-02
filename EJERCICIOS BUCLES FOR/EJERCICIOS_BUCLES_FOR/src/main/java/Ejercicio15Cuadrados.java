abbd2fc0517243b832be4cf521db78cf379a20c7
lalopgar <laralg90@gmail.com>
2025-07-02 17:06:25 +0200

    Ejercicio 15 creo el bucle con los números del 1 al 10

---
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio15Cuadrados.java
@@ -0,0 +1,10 @@
+//Mostrar los cuadrados de los números del 1 al 10. (Multiplicarlo por sí mismo (es decir, elevarlo al cuadrado))
+
+public class Ejercicio15Cuadrados {
+    public static void main(String[] args) {
+
+        for (int i=1;i<=10;i++){
+
+        }
+    }
+}
