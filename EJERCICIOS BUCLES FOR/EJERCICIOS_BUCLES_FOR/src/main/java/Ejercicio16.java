2ff4d2e2f58c1a5000a573c99c89a66f3ec66855
lalopgar <laralg90@gmail.com>
2025-07-02 17:13:55 +0200

    Ejercicio 16 imprimo los números impares

--- EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio16.java
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio16.java
@@ -4,7 +4,7 @@ public class Ejercicio16 {
     public static void main(String[] args) {
         for (int i=1;i<=20;i++){
             if (i%2!=0){
-
+                System.out.println(i);
             }
         }
     }
