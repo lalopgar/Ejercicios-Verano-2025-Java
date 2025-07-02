cc9299c12b8502604ef23bea6e984c7787e731d0
lalopgar <laralg90@gmail.com>
2025-07-02 13:54:18 +0200

    Ejercicio12

---
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio12.java
@@ -0,0 +1,9 @@
+//Contar hacia atrás del 10 al 1.
+
+public class Ejercicio12 {
+    public static void main(String[] args) {
+        for (int i =10; i>=1;i--){
+            System.out.println(i);
+        }
+    }
+}
