b2ce39ff2c21e1fb5c1c9a6ca9f7227fafdeac26
lalopgar <laralg90@gmail.com>
2025-07-02 19:37:57 +0200

    Ejercicio 9 añado columnas (por cada fila crea una columna)

--- EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio9.java
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio9.java
@@ -4,7 +4,9 @@ public class Ejercicio9 {
     public static void main(String[] args) {
 
         for (int i=1;i<=5;i++){ //filas
+            for (int j=1;j<=i;j++){  //columnas
 
+            }
         }
     }
 }
