9bd1f72690e4519fcd53884480a0bda101a201fb
lalopgar <laralg90@gmail.com>
2025-07-02 14:14:21 +0200

    Ejercicio 13 inicializo el acumulador a 0 y si el número es par se va añadiendo al acumulador

--- EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio13.java
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio13.java
@@ -3,7 +3,11 @@
 public class Ejercicio13 {
     public static void main(String[] args) {
 
+        int acumulador = 0;
         for (int i=1;i<=100;i++){
+            if (i%2==0){
+                acumulador = acumulador + i;
+            }
 
         }
     }
