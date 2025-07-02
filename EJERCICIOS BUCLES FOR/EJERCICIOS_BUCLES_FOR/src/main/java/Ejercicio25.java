c259eb38efc1007ae7bd10b59b401a16085d93af
lalopgar <laralg90@gmail.com>
2025-07-02 18:10:54 +0200

    Ejercicio 25 imprimo números pares del 1 al 50

---
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio25.java
@@ -0,0 +1,11 @@
+//Mostrar cuántos números entre 1 y 50 son pares.
+
+public class Ejercicio25 {
+    public static void main(String[] args) {
+        for (int i=1;i<=50;i++){
+            if (i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
