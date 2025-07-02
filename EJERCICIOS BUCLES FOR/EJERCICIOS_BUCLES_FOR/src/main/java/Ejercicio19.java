f02237432e8776ff73e53c23b0494205afbfce1f
lalopgar <laralg90@gmail.com>
2025-07-02 17:40:46 +0200

    Ejercicio 19 imprimo los números del 20 al 1

---
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio19.java
@@ -0,0 +1,9 @@
+//Imprime los números del 20 al 1.
+
+public class Ejercicio19 {
+    public static void main(String[] args) {
+        for (int i=20;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+}
