5a04b156ee7208185b66f7baf5b6f1c642d628c6
lalopgar <laralg90@gmail.com>
2025-07-02 17:07:57 +0200

    Ejercicio 15 multiplico cada número por él mismo y lo imprimo para obtener la solución

--- EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio15Cuadrados.java
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio15Cuadrados.java
@@ -4,7 +4,7 @@ public class Ejercicio15Cuadrados {
     public static void main(String[] args) {
 
         for (int i=1;i<=10;i++){
-
+            System.out.println(i + "x" + i + " = " + (i*i));
         }
     }
 }
