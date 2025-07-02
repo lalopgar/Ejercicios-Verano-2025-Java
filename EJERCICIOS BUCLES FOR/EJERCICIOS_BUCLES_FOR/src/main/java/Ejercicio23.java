67f91006f85349808cf47cdd231bf89a5c1d5c4f
lalopgar <laralg90@gmail.com>
2025-07-02 18:00:33 +0200

    Ejercicio 23 imprimo Hola Java 10 veces

---
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio23.java
@@ -0,0 +1,9 @@
+//Imprimir 10 veces el mensaje "Hola,Java".
+
+public class Ejercicio23 {
+    public static void main(String[] args) {
+        for (int i=1;i<=10;i++){
+            System.out.println("Hola,Java");
+        }
+    }
+}
