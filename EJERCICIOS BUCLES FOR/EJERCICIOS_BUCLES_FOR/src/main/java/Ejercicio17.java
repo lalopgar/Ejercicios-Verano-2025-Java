c4a2e4630d76a36a357de5f060140429b0be17f7
lalopgar <laralg90@gmail.com>
2025-07-02 17:18:03 +0200

    Ejercicio 17 creo las tablas hasta la del 10; 1,2,3...10

---
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio17.java
@@ -0,0 +1,9 @@
+//Imprimir la tabla de multiplicar del 1 al 10 (todas las tablas).
+
+public class Ejercicio17 {
+    public static void main(String[] args) {
+        for (int i=1;i<=10;i++){
+
+        }
+    }
+}
