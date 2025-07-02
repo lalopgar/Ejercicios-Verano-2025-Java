57de5edf931fe95e8db9a6ca2790722289c41580
lalopgar <laralg90@gmail.com>
2025-07-02 14:12:12 +0200

    Ejercicio 13, creo el bucle que va del 1 al 100

---
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio13.java
@@ -0,0 +1,10 @@
+//Mostrar la suma de los números pares del 1 al 100.
+
+public class Ejercicio13 {
+    public static void main(String[] args) {
+
+        for (int i=1;i<=100;i++){
+
+        }
+    }
+}
