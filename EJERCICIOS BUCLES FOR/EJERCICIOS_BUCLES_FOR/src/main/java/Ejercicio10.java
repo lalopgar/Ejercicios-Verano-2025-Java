e4a153968c5720abff030443ab05d7471c2ddfaf
lalopgar <laralg90@gmail.com>
2025-07-02 19:58:50 +0200

    Ejercicio 10 hago la piramide invertida

--- EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio10.java
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio10.java
@@ -1,4 +1,12 @@
 //Triángulo invertido
 
 public class Ejercicio10 {
+    public static void main(String[] args) {
+        for (int i=5;i>=1;i--){
+            for (int j=1;j<=i;j++){ //cuidado con poner j-- que se queda en bucle pillado
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
