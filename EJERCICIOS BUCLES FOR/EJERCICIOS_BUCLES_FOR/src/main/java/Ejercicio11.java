22fa1bf31c72a576d440591d43ef52fba27dd49e
lalopgar <laralg90@gmail.com>
2025-07-02 13:46:36 +0200

    Ejercicio 11 añado condicional que indica si el número recorrido es múltiplo de 5 y lo imprime

--- EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio11.java
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio11.java
@@ -3,7 +3,9 @@
 public class Ejercicio11 {
     public static void main(String[]args){
         for (int i =5;i<=50;i++){
-
+            if (i%5==0){
+                System.out.println(i);
+            }
         }
     }
 }
