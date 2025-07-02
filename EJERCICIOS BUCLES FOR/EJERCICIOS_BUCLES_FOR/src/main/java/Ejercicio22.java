cb0f0d81d0734c1123084b55fff539351a68a78c
lalopgar <laralg90@gmail.com>
2025-07-02 17:57:34 +0200

    Ejercicio 22 el doble se obtiene multiplicando el número i por 2

---
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio22.java
@@ -0,0 +1,9 @@
+//Imprimir los primeros 10 números y su doble.
+
+public class Ejercicio22 {
+    public static void main(String[] args) {
+        for (int i=1;i<=10;i++){
+            System.out.println(i + " su doble es = " +(i*2));
+        }
+    }
+}
