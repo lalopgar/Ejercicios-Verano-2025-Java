a1d506ce7674828c1692b19b6eeae0321c8d6caa
lalopgar <laralg90@gmail.com>
2025-07-03 11:37:58 +0200

    Ejercicio 27 vueltas que dará para imprimir espacios y asteriscos

--- EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio27PiramideCentrada.java
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio27PiramideCentrada.java
@@ -3,8 +3,10 @@
 public class Ejercicio27PiramideCentrada {
     public static void main(String[] args) {
 
-        int alturaPiramide = 5;
+        int alturaPiramide = 5; //Va a ser de 5 pisos
 
+        for (int i=0;i<alturaPiramide;i++){ //las vueltas q dará para imprimir los espacios y asteriscos. De 0 a 4 van 5
 
+        }
     }
 }
