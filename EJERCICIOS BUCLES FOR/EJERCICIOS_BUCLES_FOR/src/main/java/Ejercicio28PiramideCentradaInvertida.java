842ad2ca0eadda21565c710707ef05a20592232d
lalopgar <laralg90@gmail.com>
2025-07-03 12:43:00 +0200

    Ejercicio 28 astericos decreciendo

--- EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio28PiramideCentradaInvertida.java
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio28PiramideCentradaInvertida.java
@@ -7,8 +7,12 @@ public class Ejercicio28PiramideCentradaInvertida {
 
         for (int i = 0; i < alturaPiramide; i++){
             for (int j = 0; j < i; j++){  //espacios que aumenta de 1 en 1 conforme baja 0,1,2,3,4
-                System.out.println(" ");
+                System.out.print(" ");
             }
+            for (int k = 0; k < 2 * (alturaPiramide-i) -1; k++){ //Asteriscos formula para decrecerlos 2 * (alturaPiramide-i) -1
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
