0591e7cb107849f79943b46c2a15c8609a9e46a8
lalopgar <laralg90@gmail.com>
2025-07-04 14:04:43 +0200

    Ejercicio 28 piramide centrada invertida le añado al comentario el número de asteriscos que imprimiria

--- EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio28PiramideCentradaInvertida.java
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio28PiramideCentradaInvertida.java
@@ -9,7 +9,7 @@ public class Ejercicio28PiramideCentradaInvertida {
             for (int j = 0; j < i; j++){  //espacios que aumenta de 1 en 1 conforme baja 0,1,2,3,4
                 System.out.print(" ");
             }
-            for (int k = 0; k < 2 * (alturaPiramide-i) -1; k++){ //Asteriscos formula para decrecerlos 2 * (alturaPiramide-i) -1
+            for (int k = 0; k < 2 * (alturaPiramide-i) -1; k++){ //Asteriscos formula para decrecerlos 2 * (alturaPiramide-i) -1  (9,7,5,3,1)
                 System.out.print("*");
             }
             System.out.println();
---
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio34PiramideCentradaInvertidaRepaso.java
@@ -0,0 +1,15 @@
+public class Ejercicio34PiramideCentradaInvertidaRepaso {
+    public static void main(String[] args) {
+
+        int altura = 5;
+        for (int i = 0; i <= altura; i++){
+            for (int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 5; k > 2*i-1; k--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
