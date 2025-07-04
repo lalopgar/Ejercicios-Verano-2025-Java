1f6b0c3b0026c97d623710a69c010d85ebaa083f
lalopgar <laralg90@gmail.com>
2025-07-04 12:00:03 +0200

    Ejercicio 31 pirámide a la izq invertida

---
+++ EJERCICIOS BUCLES FOR/EJERCICIOS_BUCLES_FOR/src/main/java/Ejercicio31PiramideIzqInvertida.java
@@ -0,0 +1,12 @@
+public class Ejercicio31PiramideIzqInvertida {
+    public static void main(String[] args) {
+
+        int altura = 5;
+        for (int i = 1; i<= altura; i++){
+            for (int j = 5; j >= i; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
