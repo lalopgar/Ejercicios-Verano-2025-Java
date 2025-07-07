2c070edb086facdb67adc2ad19eafbedc20d98fb
lalopgar <laralg90@gmail.com>
2025-07-07 11:43:17 +0200

    Ejercicio 3 abro Scanner

---
+++ EJERCICIOS BUCLES/EJERCICIOS_BUCLES_WHILE/src/main/java/Ejercicio3.java
@@ -0,0 +1,12 @@
+//Pide al usuario un número N y calcula la suma de los números del 1 al N
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+
+    }
+}
