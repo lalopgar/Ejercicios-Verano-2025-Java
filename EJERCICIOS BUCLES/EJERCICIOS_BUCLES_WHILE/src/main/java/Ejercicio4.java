1291e380f813af34ae8def75fa21dcde26a66607
lalopgar <laralg90@gmail.com>
2025-07-07 12:28:28 +0200

    Ejercicio 4 hacemos un condicional para que si el num secreto es diferente de 5 nos avise con otro condicional anidado si éste num introducido es menor o mayor que 5 y así sea más fácil adivinarlo. Si se adivina se sale del bucle e imprime que lo hemos adivinado.

--- EJERCICIOS BUCLES/EJERCICIOS_BUCLES_WHILE/src/main/java/Ejercicio4.java
+++ EJERCICIOS BUCLES/EJERCICIOS_BUCLES_WHILE/src/main/java/Ejercicio4.java
@@ -14,7 +14,16 @@ public class Ejercicio4 {
             System.out.println("Introduce número secreto: ");
             num = teclado.nextInt();
 
+            if (num != 5){
+                if (num < 5){
+                    System.out.println("Inténtalo de nuevo.El número secreto es mayor que el introducido.");
+                }else{
+                    System.out.println("Inténtalo de nuevo.El número secreto es menor que el introducido.");
+                }
+            }
 
         }
+        System.out.println("Felicidades, el número secreto es " + numSecreto);
+        teclado.close();
     }
 }
