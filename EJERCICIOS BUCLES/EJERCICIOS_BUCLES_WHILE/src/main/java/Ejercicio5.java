96d7d1544936054ea66947b3681ec6018a464814
lalopgar <laralg90@gmail.com>
2025-07-07 13:01:31 +0200

    Ejercicio 5 abro scanner y pregunto por el num

---
+++ EJERCICIOS BUCLES/EJERCICIOS_BUCLES_WHILE/src/main/java/Ejercicio5.java
@@ -0,0 +1,15 @@
+//Pide un número positivo y muestra la cuenta regresiva hasta 0.
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce un número positivo: ");
+        int num = teclado.nextInt();
+
+
+    }
+}
