bca422c54bdb20a802e3badb46a364ca7243bfc2
lalopgar <laralg90@gmail.com>
2025-07-07 13:52:48 +0200

    Ejercicio 6 abro teclado y inicializo la variable num

---
+++ EJERCICIOS BUCLES/EJERCICIOS_BUCLES_WHILE/src/main/java/Ejercicio6.java
@@ -0,0 +1,14 @@
+//Crea un menú que se repita hasta que el usuario elija salir.
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        int num = 0;
+
+
+    }
+}
