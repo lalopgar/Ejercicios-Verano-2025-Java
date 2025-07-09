99618620e223197a061bbd248b8393a1f17cecd7
lalopgar <laralg90@gmail.com>
2025-07-09 11:55:50 +0200

    Ejercicio 9 añado scanner y variables

---
+++ EJERCICIOS BUCLES/EJERCICIOS_BUCLES_WHILE/src/main/java/Ejercicio9.java
@@ -0,0 +1,17 @@
+//El programa pide una contraseña al usuario.Tiene hasta 3 intentos para adivinarla.
+//Si la acierta, muestra un mensaje de éxito.Si falla las tres veces, muestra un mensaje de acceso denegado.
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        String password;
+        String realPassword = "java1234";
+        int contador = 0;
+
+
+    }
+}
