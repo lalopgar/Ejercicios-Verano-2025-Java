cdd374fa9bee711ce77cacadabcfcc492b97ed9e
lalopgar <laralg90@gmail.com>
2025-07-09 10:29:30 +0200

    Ejercicio 8 abro scanner, declaro variables y digo que inserte un num

---
+++ EJERCICIOS BUCLES/EJERCICIOS_BUCLES_WHILE/src/main/java/Ejercicio8.java
@@ -0,0 +1,18 @@
+//El usuario introduce números enteros hasta que introduce -1.
+//Al finalizar, el programa muestra la media de todos los números introducidos(sin contar el -1).
+
+import java.util.Scanner;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+
+        Scanner teclado =new Scanner(System.in);
+
+        int num;
+        int acumulador = 0;
+        int contador = 0;
+
+        System.out.println("Introduce un número.Pulsa -1 para salir del programa.");
+        num = teclado.nextInt();
+    }
+}
