49761faabdbbf8a1d0a91e300b1991ccb4808388
lalopgar <laralg90@gmail.com>
2025-07-14 13:15:11 +0200

    Ejercicio 1 añadido

---
+++ EJERCICIOS BUCLES/EJERCICIOS_BUCLES_DO_WHILE/src/main/java/Ejercicio1.java
@@ -0,0 +1,13 @@
+//Imprimir los números del 1 al 10
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        do{
+            System.out.println("Vuelta número " + contador);
+            contador++;
+        }while(contador <= 10);
+    }
+}
