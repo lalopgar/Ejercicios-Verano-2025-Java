2872704d37bf86e6d02111b1438bf90fc8c046b5
lalopgar <laralg90@gmail.com>
2025-07-14 13:24:45 +0200

    Ejercicio 2 scanner y declaracion de variales

---
+++ EJERCICIOS BUCLES/EJERCICIOS_BUCLES_DO_WHILE/src/main/java/Ejercicio2.java
@@ -0,0 +1,15 @@
+//Pedir al usuario que ingrese números y sumarlos. El programa se detiene cuando el usuario ingresa 0.
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        int acumulador = 0;
+        int num;
+
+
+    }
+}
