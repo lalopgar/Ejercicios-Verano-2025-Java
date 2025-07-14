eaba7ea7108f4fccced86af861959db599901143
lalopgar <laralg90@gmail.com>
2025-07-14 13:40:33 +0200

    Ejercicio 3 añado scanner y variable

---
+++ EJERCICIOS BUCLES/EJERCICIOS_BUCLES_DO_WHILE/src/main/java/Ejercicio3.java
@@ -0,0 +1,14 @@
+//Pedir al usuario que ingrese una contraseña hasta que escriba la correcta
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        String contrasena = "lara1234";
+
+
+    }
+}
