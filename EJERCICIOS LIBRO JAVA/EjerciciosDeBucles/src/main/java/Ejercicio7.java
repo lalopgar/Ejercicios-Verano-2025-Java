5e9b80d8322bf4625befeeebd50b4ab99146a033
lalopgar <laralg90@gmail.com>
2025-07-15 20:06:11 +0200

    Ejercicio 7 añado condicional que indica que se ha fallado 4 veces, corrijo ; de la pregunta y corrijo un break que faltaba en el prime condicional para que acabe el programa si acierto la contraseña

--- EJERCICIOS LIBRO JAVA/EjerciciosDeBucles/src/main/java/Ejercicio7.java
+++ EJERCICIOS LIBRO JAVA/EjerciciosDeBucles/src/main/java/Ejercicio7.java
@@ -17,16 +17,21 @@ public class Ejercicio7 {
         int i = 0;
 
         while(i < 4){
-            System.out.println(System.out.println("Introduce la contraseña numérica correcta. Tienes " + acumulador + " oportunidades: ");
+            System.out.println("Introduce la contraseña numérica correcta. Tienes " + acumulador + " oportunidades: ");
             num = teclado.nextInt();
             acumulador = acumulador - 1;
 
             if (num == 1234){
                 System.out.println("La caja fuerte se ha abierto satisfactoriamente.");
+                break;
             }else{
                 System.out.println("Lo siento, esa no es la combinación.");
             }
             i++;
         }
+        if (i == 4){
+            System.out.println("Lo siento, has fallado 4 veces.");
+        }
+        teclado.close();
     }
 }
