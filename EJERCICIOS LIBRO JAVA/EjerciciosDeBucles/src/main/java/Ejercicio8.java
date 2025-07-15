488f220c3888d3f6082940c68b6ad4bd744bb118
lalopgar <laralg90@gmail.com>
2025-07-15 20:11:18 +0200

    Ejercicio 8 añado scanner

---
+++ EJERCICIOS LIBRO JAVA/EjerciciosDeBucles/src/main/java/Ejercicio8.java
@@ -0,0 +1,14 @@
+//Muestra la tabla de multiplicar de un número introducido por teclado.
+
+import java.util.Scanner;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce un número del 1 al 10: ");
+        int num = teclado.nextInt();
+
+
+    }
+}
