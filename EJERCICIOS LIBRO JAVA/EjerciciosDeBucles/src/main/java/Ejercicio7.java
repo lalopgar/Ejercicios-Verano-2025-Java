dddacafa8decc5de4f3da41fe694e59186f02eeb
lalopgar <laralg90@gmail.com>
2025-07-15 19:51:13 +0200

    Ejercicio 7 añado scanner y variables

---
+++ EJERCICIOS LIBRO JAVA/EjerciciosDeBucles/src/main/java/Ejercicio7.java
@@ -0,0 +1,20 @@
+//Realiza el control de acceso a una caja fuerte. La combinación será un
+//número de 4 cifras. El programa nos pedirá la combinación para abrirla. Si no
+//acertamos, se nos mostrará el mensaje “Lo siento, esa no es la combinación”
+//y si acertamos se nos dirá “La caja fuerte se ha abierto satisfactoriamente”.
+//Tendremos cuatro oportunidades para abrir la caja fuerte.
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        int contrasena = 1234;
+        int acumulador = 4;
+        int i = 0;
+
+
+    }
+}
